88b16ac6a5c943937eb5c00ce9a3d596c9244b81
Eugene Fedorenko <fedorenko@gmail.com>
2021-09-01 19:11:02 -0400

    Added some types coverage

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,7 @@
-export const roundTo100th = num => roundTo(num, 100);
-export const roundTo10th = num => roundTo(num, 10);
-export const roundToWhole = num => roundTo(num, 1);
-const roundTo = (num, multiplier) => Math.round(num * multiplier) / multiplier;
+const roundTo = (num: number, multiplier: number): number => Math.round(num * multiplier) / multiplier;
+
+export const roundTo100th = (num: number): number => roundTo(num, 100);
+
+export const roundTo10th = (num: number): number => roundTo(num, 10);
+
+export const roundToWhole = (num: number): number => roundTo(num, 1);
--- src/stores.ts
+++ src/stores.ts
@@ -1,76 +1,101 @@
-import { writable, readable, get } from 'svelte/store';
+import { get, readable, writable } from 'svelte/store';
 
-export const defaultBgColor = readable('#FFF');
-export const bgColor = writable(get(defaultBgColor));
-export const selectedColor = writable();
+/* Background Color & Contrast Check
+---------------------------------------- */
 
-export const lightnessSteps = writable({
-	50: 98,
-	100: 93.3,
-	200: 88.6,
-	300: 79.9,
-	400: 71.2,
-	500: 60.5,
-	600: 49.8,
-	700: 38.4,
-	800: 27.0,
-	900: 15.6
-});
+export const defaultBgColor = readable<string>('#FFF');
+export const bgColor = writable<string>(get(defaultBgColor));
+
+
+/* Lightness
+---------------------------------------- */
+
+export interface LightnessInterface {
+  50: number;
+  100: number;
+  200: number;
+  300: number;
+  400: number;
+  500: number;
+  600: number;
+  700: number;
+  800: number;
+  900: number;
+}
+const _lightnessSteps: LightnessInterface = {
+  50: 98,
+  100: 93.3,
+  200: 88.6,
+  300: 79.9,
+  400: 71.2,
+  500: 60.5,
+  600: 49.8,
+  700: 38.4,
+  800: 27.0,
+  900: 15.6
+};
+export const lightnessSteps = writable(_lightnessSteps);
+
+
+/* Base Colors
+---------------------------------------- */
 
 // TODO: remove pre-defined names
-export const baseColors = writable([
-	{
-		name: 'red',
-		color: '#fe6f5c',
-		isLab: true,
-		hueCorrection: 0
-	},
-	{
-		name: 'orange',
-		color: '#f59432',
-		isLab: true,
-		hueCorrection: -10
-	},
-	{
-		name: 'yellow',
-		color: '#e3d90b',
-		isLab: true,
-		hueCorrection: -15
-	},
-	{
-		name: 'green',
-		color: '#44c74c',
-		isLab: false,
-		hueCorrection: 0
-	},
-	{
-		name: 'blue',
-		color: '#0088c9',
-		isLab: false,
-		hueCorrection: 0
-	},
+
+export interface BaseColor {
+  name: string;
+  color: string;
+  isLab: boolean;
+  hueCorrection: number;
+}
+const _baseColors: BaseColor[] = [
+  {
+    name: 'red',
+    color: '#fe6f5c',
+    isLab: true,
+    hueCorrection: 0
+  },
+  {
+    name: 'orange',
+    color: '#f59432',
+    isLab: true,
+    hueCorrection: -10
+  },
+  {
+    name: 'yellow',
+    color: '#e3d90b',
+    isLab: true,
+    hueCorrection: -15
+  },
+  {
+    name: 'green',
+    color: '#44c74c',
+    isLab: false,
+    hueCorrection: 0
+  },
+  {
+    name: 'blue',
+    color: '#0088c9',
+    isLab: false,
+    hueCorrection: 0
+  },
   {
     name: 'purple',
     color: '#397ae5',
     isLab: false,
     hueCorrection: 15
   },
-	{
-		name: 'slate',
-		color: '#738097',
-		isLab: false,
-		hueCorrection: 0
-	},
-	// {
-	// 	name: 'beige',
-	// 	color: '#EAE8DE',
-	// 	isLab: false,
-	// 	hueCorrection: 0
-	// },
-	{
-		name: 'grey',
-		color: '#808080',
-		isLab: false,
-		hueCorrection: 0
-	}
-]);
+  {
+    name: 'slate',
+    color: '#738097',
+    isLab: false,
+    hueCorrection: 0
+  },
+  {
+    name: 'grey',
+    color: '#808080',
+    isLab: false,
+    hueCorrection: 0
+  }
+];
+export const baseColors = writable(_baseColors);
