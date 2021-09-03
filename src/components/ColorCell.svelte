1821a2973661e4aa49fdd72d721ad8134c316a4b
Eugene Fedorenko <fedorenko@gmail.com>
2021-09-02 23:23:26 -0400

    Better text colors

--- src/components/ColorCell.svelte
+++ src/components/ColorCell.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
   import chroma from 'chroma-js';
-  import { bgColor, defaultBgColor } from '../stores';
+  import { bgColor, defaultBgColor, lightnessShades } from '../stores';
   import { roundTo10th } from '../helpers';
   import type { Palette } from '../generatePalette';
 
   export let color: Palette, shade: string;
+
+  $: lightness = $lightnessShades[shade] as number;
 
   let c: number = roundTo10th(chroma(color.color).get('lch.c'));
   let h: number = roundTo10th(chroma(color.color).get('lch.h'));
@@ -27,7 +29,10 @@
 <div
   style="background-color: {color.color};"
   on:click={selectColor}
-  class={isSelected ? 'selected' : ''}
+  class={[
+    isSelected ? 'selected' : '',
+    lightness >= 60 ? 'dark' : 'light'
+  ].join(' ')}
   data-shade={shade}
   title="Chroma: {c}, Hue: {h}"
 >
@@ -46,60 +51,13 @@
     text-align: center;
     cursor: pointer;
   }
-
-  /* TODO: Lightness levels can vary, so instead we should create CSS vars behind the scenes */
-  /* Maintain 50 CR */
-  [data-shade='50'] {
-    color: #868686;
-    color: rgba(0, 0, 0, 0.47);
+  .light {
+    color: rgba(255, 255, 255, .66);
   }
-
-  [data-shade='100'] {
-    color: #777;
-    color: rgba(0, 0, 0, 0.53);
+  .dark {
+    color: rgba(0, 0, 0, .66);
   }
-
-  [data-shade='200'] {
-    color: #646464;
-    color: rgba(0, 0, 0, 0.61);
-  }
-
-  [data-shade='300'] {
-    color: #444;
-    color: rgba(0, 0, 0, 0.73);
-  }
-
-  [data-shade='400'] {
-    color: #101010;
-    color: rgba(0, 0, 0, 0.94);
-  }
-
-  [data-shade='500'] {
-    color: #f9f9f9;
-    color: rgba(255, 255, 255, 0.98);
-  }
-
-  [data-shade='600'] {
-    color: #e4e4e4;
-    color: rgba(255, 255, 255, 0.89);
-  }
-
-  [data-shade='700'] {
-    color: #d2d2d2;
-    color: rgba(255, 255, 255, 0.82);
-  }
-
-  [data-shade='800'] {
-    color: #c2c2c2;
-    color: rgba(255, 255, 255, 0.76);
-  }
-
-  [data-shade='900'] {
-    color: #b6b6b6;
-    color: rgba(255, 255, 255, 0.71);
-  }
-
   .selected {
-    box-shadow: inset 0 0 0 3px rgba(255, 255, 255, 0.75);
+    box-shadow: inset 0 0 0 3px rgba(255, 255, 255, .75);
   }
 </style>
